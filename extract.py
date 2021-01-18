23053a74c251413ae896c6223bcca7c74bd90551
Pythalex <hyorvenn@protonmail.com>
2021-01-18 14:09:23 +0100

    Fixed newline bug when parsing train.txt

--- extract.py
+++ extract.py
@@ -1,12 +1,18 @@
+# -*- coding:utf-8 -*-
+
 import pandas as pd
 
 
 def create_df(path) :
-    f = open(path,'r')
+    f = open(path,'r', encoding="utf-8")
     lines = f.readlines()
     f.close()
     l = []
     for line in lines :
+        
+        # remove \n character
+        line = line.rstrip()
+
         tweet = line.split(" ",1)
         body = tweet[1]
         meta_data = tweet[0][1:-1].split(",")
