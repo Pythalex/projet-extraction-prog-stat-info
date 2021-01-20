e93d1d6aeee701a0e3830a81f0c79e46a02a91e9
Pythalex <hyorvenn@protonmail.com>
2021-01-20 17:18:58 +0100

    Renamed transformer2077, added if name = main for test

---
+++ transformer/SplitterPunctuation.py
@@ -0,0 +1,28 @@
+from sklearn.base import TransformerMixin
+
+
+class SplitterPunctuation(TransformerMixin):
+
+    def __init__(self, ponct=r"[, \-!?:]+"):
+        self.ponct = ponct
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        X = X.apply(str.lower)
+        X = X.str.split(self.ponct)
+        return X
+
+    def fit_transform(self, X, y=None):
+        return self.fit(X, y).transform(X)
+
+
+if __name__ == "__main__":
+
+    import pandas as pd
+
+    tweets = pd.read_csv("../train_proper.csv")
+    transformer = SplitterPunctuation()
+    print(transformer.transform(tweets["body"]).head(5))
